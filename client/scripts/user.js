d57a3a41971e0b84667dfd385b3bb0c5aae26e66
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-11-20 07:38:44 -0600

    form works, db not connected

---
+++ client/scripts/user.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log("Hey it loads!");
+
+    $.ajax({
+        type: "GET",
+        url: "/user",
+        success: function(data){
+            console.log(data);
+            $("#welcome").text("Welcome, " +  data.username);
+            $(".display").text(working);
+        }
+    });
+});
